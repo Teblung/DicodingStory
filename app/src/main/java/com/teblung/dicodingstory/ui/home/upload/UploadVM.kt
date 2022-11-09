5c72722b41f7008cffc4339e6931d0cda6c5f55f
Theo Sepdianto <theo.sepdianto97@gmail.com>
2022-11-09 11:58:58 +0700

    Add hilt view model

--- app/src/main/java/com/teblung/dicodingstory/ui/home/upload/UploadVM.kt
+++ app/src/main/java/com/teblung/dicodingstory/ui/home/upload/UploadVM.kt
@@ -3,9 +3,11 @@ package com.teblung.dicodingstory.ui.home.upload
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.teblung.dicodingstory.data.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import java.io.File
 import javax.inject.Inject
 
+@HiltViewModel
 class UploadVM @Inject constructor(private val repository: Repository) : ViewModel() {
     val message: LiveData<String> = repository.message
     val loading: LiveData<Boolean> = repository.isLoading
