8527396547d2099ff3d639b105d64e31ba4a4175
Theo Sepdianto <theo.sepdianto97@gmail.com>
2022-11-13 02:07:56 +0700

    Add type data

--- app/src/main/java/com/teblung/dicodingstory/data/source/local/entity/RemoteEntity.kt
+++ app/src/main/java/com/teblung/dicodingstory/data/source/local/entity/RemoteEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "remote")
-class RemoteEntity(
+data class RemoteEntity(
     @PrimaryKey val id: String,
     val prevKey: Int?,
     val nextKey: Int?
